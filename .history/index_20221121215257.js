e1884be795a2e911cde028c604a19bda0276064d
patelamancode <74897715+patelamancode@users.noreply.github.com>
2022-11-21 22:42:50 +0530

    feat : adding error feature and final submission done

--- .history/index_20221121215257.js
+++ .history/index_20221121215257.js
@@ -1,3 +1,7 @@
+// document.querySelector('.check-btn').addEventListener('click', function(){
+//     console.log("Check button is clicked")
+// })
+
 const billAmount = document.querySelector('#bill-amount');
 const cashAmount = document.querySelector('#cash-amount');
 const checkButton = document.querySelector('.check-btn');
@@ -6,20 +10,14 @@ const notesDisplay = document.querySelectorAll('.notes-number');
 const hideContent = document.querySelector('.sub-container');
 const nextButton = document.querySelector('#next-btn');
 const mainError = document.querySelector('.error-check-for-bill');
-const notesArray = [2000, 1000, 500, 200, 100, 50, 20, 10, 5, 1];
 
-nextButton.addEventListener('click', function () {
-    if (!billAmount.value) {
+
+nextButton.addEventListener('click', function(){
+    hideContent.style.display = "flex";
+    nextButton.style.display = "none";
+    if(!billAmount.value){
         hideContent.style.display = "none";
-        mainError.style.display = "block";
-        mainError.innerHTML = "Please enter a bill value";
-    } else if (billAmount.value < 0) {
-        hideContent.style.display = "none";
-        mainError.style.display = "block";
-        mainError.innerHTML = "Please enter a positive bill value";
-    } else {
-        mainError.style.display = "none";
-        hideContent.style.display = "flex";
+        mainError.style.display = "block"
     }
 });
 
@@ -28,16 +26,17 @@ const displayErrorMessage = function (message) {
     errorMessage.style.backgroundColor = "red";
     errorMessage.innerText = message;
 };
-const hideErrorMessage = function () {
+const hideErrorMessage = function(){
     errorMessage.style.display = "none";
 
 };
 
-const numberOfNotesGiven = function (returnAmount) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let notesCount = Math.trunc(returnAmount / notesArray[i]);
-        returnAmount = returnAmount % notesArray[i];
-        notesDisplay[i].innerText = notesCount;
+const notesArray = [2000, 1000, 500, 200, 100, 50, 20, 10, 5, 1];
+const numberOfNotesGiven = function(returnAmount){
+    for(let i=0; i< notesArray.length; i++){
+        let notesCount = Math.trunc(returnAmount/notesArray[i]);
+        returnAmount = returnAmount%notesArray[i];
+        notesDisplay[i].innerText = notesCount; 
     }
 };
 
@@ -45,15 +44,18 @@ checkButton.addEventListener('click', function amountValidation() {
     hideErrorMessage();
 
     if (billAmount.value > 0) {
-        if (cashAmount.value > billAmount.value) {
+        if(cashAmount.value > billAmount.value){
             const returnAmount = cashAmount.value - billAmount.value;
             numberOfNotesGiven(returnAmount);
-        } else if (cashAmount.value === billAmount.value) {
+        }
+        else if(cashAmount.value === billAmount.value){
             displayErrorMessage("AMOUNTS ARE EQUALS NO NEED FOR CHANGE")
-        } else {
+        }
+        else{
             displayErrorMessage("Invalid Cash value as per Bill amount");
         }
-    } else {
+    } 
+    else {
         displayErrorMessage("Please make sure input bill amount must be positive and non-zero value");
     }
 });
